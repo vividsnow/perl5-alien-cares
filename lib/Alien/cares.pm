d4a78f3d13ae6622ea59b672247ef79f2103460d
vividsnow <vividsnow@gmail.com>
2026-05-06 15:50:05 +0300

    release 0.02
    - Drop _add_rpath override and the libs/libs_static wrappers; share build
      forces static linkage (CARES_SHARED=OFF) so injecting -Wl,-rpath was a
      no-op
    - Polish POD: prefer Alien::Base::Wrapper in the synopsis, structure the
      SEE ALSO list, add COPYRIGHT AND LICENSE, mention ALIEN_INSTALL_TYPE
    - Regenerate README from POD; bump VERSION to 0.02
    - ci: FreeBSD package renamed libcares -> c-ares

--- lib/Alien/cares.pm
+++ lib/Alien/cares.pm
@@ -3,18 +3,7 @@ use strict;
 use warnings;
 use parent 'Alien::Base';
 
-our $VERSION = '0.01';
-
-sub _add_rpath {
-    my ($self, $libs) = @_;
-    if ($^O eq 'darwin' && $self->install_type eq 'share' && $libs =~ /-L(\S+)/) {
-        return "-Wl,-rpath,$1 $libs";
-    }
-    return $libs;
-}
-
-sub libs        { $_[0]->_add_rpath($_[0]->SUPER::libs) }
-sub libs_static { $_[0]->_add_rpath($_[0]->SUPER::libs_static) }
+our $VERSION = '0.02';
 
 1;
 
@@ -22,60 +11,95 @@ __END__
 
 =head1 NAME
 
-Alien::cares - Find or build the c-ares async DNS resolver library
+Alien::cares - find or build the c-ares asynchronous DNS resolver library
 
 =head1 SYNOPSIS
 
-In your F<Makefile.PL>:
+In an XS distribution's F<Makefile.PL>, using L<Alien::Base::Wrapper>:
 
-    use Alien::cares;
     use ExtUtils::MakeMaker;
+    use Alien::Base::Wrapper qw( Alien::cares !export );
 
     WriteMakefile(
-        ...
-        CONFIGURE_REQUIRES => { 'Alien::cares' => 0 },
-        INC  => Alien::cares->cflags,
-        LIBS => [Alien::cares->libs],
+        Alien::Base::Wrapper->mm_args,
+        NAME               => 'My::Module',
+        CONFIGURE_REQUIRES => {
+            'Alien::Base::Wrapper' => 0,
+            'Alien::cares'         => 0,
+        },
+        PREREQ_PM => { 'Alien::cares' => 0 },
     );
 
-In your F<alienfile> (if wrapping for another Alien):
+Or, accessing the flags directly:
+
+    use Alien::cares;
+    INC  => Alien::cares->cflags,
+    LIBS => [ Alien::cares->libs ],
+
+In another distribution's F<alienfile>:
 
     use alienfile;
     share { requires 'Alien::cares' };
 
 =head1 DESCRIPTION
 
-Alien::cares finds or builds the L<c-ares|https://c-ares.org/> C library
-for asynchronous DNS resolution.
+Alien::cares finds or builds the L<c-ares|https://c-ares.org/> C library,
+which provides asynchronous DNS resolution.  The dual install strategy
+follows the L<Alien::Base> convention:
 
 =over 4
 
 =item *
 
-If a system-installed c-ares E<gt>= 1.22.0 is detected via C<pkg-config>,
-it is used directly.
+A system-installed c-ares E<gt>= 1.22.0 detected via C<pkg-config> is
+used directly (I<system> install).
 
 =item *
 
-Otherwise the latest release is downloaded from GitHub and built with
-CMake as a static library.
+Otherwise the latest release tarball is downloaded from GitHub and built
+with CMake as a static library bundled with this distribution
+(I<share> install).
 
 =back
 
+Set the C<ALIEN_INSTALL_TYPE> environment variable to C<system> or
+C<share> to force a particular install type.
+
 =head1 METHODS
 
-Inherited from L<Alien::Base>.  On macOS share installs, C<libs> and
-C<libs_static> inject C<-Wl,-rpath> for the dynamic linker.
+This module inherits all methods from L<Alien::Base>.  The most useful
+ones are C<cflags>, C<libs>, C<libs_static>, C<version>, and
+C<install_type>.
 
 =head1 SEE ALSO
 
-L<Alien::Base>, L<EV::cares>, L<https://c-ares.org/>
+=over 4
+
+=item *
+
+L<Alien::Base> - the framework this module is built on
+
+=item *
+
+L<Alien::Build::Manual::AlienUser> - guide to consuming an Alien
+
+=item *
+
+L<EV::cares> - libev-based Perl bindings to c-ares
+
+=item *
+
+L<https://c-ares.org/> - upstream project
+
+=back
 
 =head1 AUTHOR
 
 vividsnow
 
-=head1 LICENSE
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2026 by vividsnow.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
